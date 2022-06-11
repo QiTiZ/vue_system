bba8d1c9263dc1ed80f41473d0ecce98acf636d2
QiTiz <qitiz666@163.com>
2022-06-11 12:53:08 +0800

    登录功能完成

--- src/main.js
+++ src/main.js
@@ -3,10 +3,34 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import axios from 'axios'
+
+// 导入 element
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/assets/css/global.less'
+
+axios.defaults.baseURL = 'http://leju.bufan.cloud/lejuClient/'
+
+// 请求拦截
+axios.interceptors.request.use((data) => {
+  data.headers.Authorization = window.sessionStorage.getItem('Token')
+  return data
+})
+
+// 响应拦截
+axios.interceptors.response.use((config) => {
+  return config
+})
+
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
+
+Vue.use(ElementUI)
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="login">
+    <!-- 头像区域 -->
+    <div class="avator">
+      <el-avatar
+        :size="150"
+        src="https://pic.imgdb.cn/item/62a3fba609475431292da0f9.jpg"
+      ></el-avatar>
+    </div>
+
+    <!-- 登录表格区域 -->
+    <div class="login_form">
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="loginFormRef"
+        label-width="0px"
+      >
+        <el-form-item prop="username">
+          <el-input
+            prefix-icon="el-icon-user-solid"
+            v-model="ruleForm.username"
+            placeholder="Username"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            show-password
+            prefix-icon="el-icon-lock"
+            v-model="ruleForm.password"
+            placeholder="Password"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+
+      <el-button type="primary" @click="goHome">Login</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {
+      // 登录表单数据绑定对象
+      ruleForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 8, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 3, max: 8, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  created() {},
+  methods: {
+    goHome() {
+      this.$refs.loginFormRef.validate(async (valid) => {
+        if (!valid) return ''
+
+        const { data: res } = await this.$http.post(
+          'login/doLogin',
+          this.ruleForm
+        )
+
+        if (res.success === 'false') {
+          return this.$message.error('登录失败')
+        }
+
+        this.$message.success('登录成功')
+
+        window.sessionStorage.setItem('Token', res.data.token)
+
+        this.$router.push('/home')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login {
+  position: relative;
+  background-color: #2d3a4b;
+}
+
+.avator {
+  position: absolute;
+  left: 50%;
+  top: 30%;
+  z-index: 999;
+  transform: translate(-50%, -50%);
+}
+
+.login_form {
+  position: absolute;
+  padding: 0 15px;
+  left: 50%;
+  top: 52%;
+  transform: translate(-50%, -50%);
+  border-radius: 15px;
+  border: 1px solid #fff;
+  width: 430px;
+  height: 270px;
+  .el-form {
+    margin-top: 90px;
+  }
+  ::v-deep .el-input__inner {
+    background-color: #283443;
+    color: #fff;
+    border: 1px solid #555;
+  }
+  .el-button {
+    width: 100%;
+  }
+}
+</style>
